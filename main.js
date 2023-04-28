1ce8f576608a5d86db3722a0e4bce04e857318c4
sheshardrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2023-04-28 20:40:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function hi(){
+    window.open("https://sheshardrinivas.github.io/ELON-MUSK/ ");
+
+
+
+}
+function ho(){
+    window.open("https://sheshardrinivas.github.io/albert-2.0/");
+    
+    
+    
+    }
+    function h4(){
+    
+        window.open("https://sheshardrinivas.github.io/da/");
+
+        }
+        
+        
+        
